323a511ace6ae269adb9a8599761b92dcec2f2b9
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-01-07 22:55:06 -0500

    commit tree d2ae1cee044704762caba9c95f22a65a3064cfeb

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, "")
+  }
+
+  static titleize(string) {
+    let sans = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    
+    let splitString = string.split(' ').map(word => {
+      if(!sans.includes(word)){
+        return Formatter.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(' ')
+    return Formatter.capitalize(splitString)
+  }
 }
